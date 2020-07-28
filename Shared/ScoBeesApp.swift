52f8f26be3e06575e69a3276f8e9ab8124deea34
Jieying Yang <jieying.yang@cca.edu>
2020-07-28 00:48:57 -0700

    Initial Commit

---
+++ Shared/ScoBeesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoBeesApp.swift
+//  Shared
+//
+//  Created by Jieying Yang on 7/28/20.
+//
+
+import SwiftUI
+
+@main
+struct ScoBeesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
